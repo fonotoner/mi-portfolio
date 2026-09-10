a8d841edd2121ff26c4f9e31965c8e6c5684e591
fon-ros <alfonso@ros.es>
2026-09-10 13:11:14 +0200

    Cambios en la home

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,7 @@ export default {
 	],
 	darkMode: 'class',
 	theme: {
-		extend: {
-			animation: {
-				'pulse-fade-in': 'pulse-fade-in 1s ease-in-out forwards',
-			},
-			keyframes: {
-				'pulse-fade-in': {
-					'0%': { opacity: '0', transform: 'scale(0.95)' },
-					'50%': { opacity: '1' },
-					'100%': { opacity: '1', transform: 'scale(1)' },
-				},
-			},
-		},
+		extend: {},
 	},
 	plugins: [
 		require("tailwindcss-animated"),
